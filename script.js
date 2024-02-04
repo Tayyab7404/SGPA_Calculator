b2e06d50f23e61af63f8121ebf49d0f4146c5c55
Tayyab <98115384+Tayyab7404@users.noreply.github.com>
2024-02-05 00:32:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,98 @@
 function calculateSem1GPA()
 {
-	const sub1 = parseFloat(document.getElementById("S1").value);
-	const sub2 = parseFloat(document.getElementById("S2").value);
-	const sub3 = parseFloat(document.getElementById("S3").value);
-	const sub4 = parseFloat(document.getElementById("S4").value);
-	const sub5 = parseFloat(document.getElementById("S5").value);
-	const sub6 = parseFloat(document.getElementById("S6").value);
-	const sub7 = parseFloat(document.getElementById("S7").value);
-	const sub8 = parseFloat(document.getElementById("S8").value);
-	const sub9 = parseFloat(document.getElementById("S9").value);
+	const sub1 = document.getElementById("S1").value;
+	const sub2 = document.getElementById("S2").value;
+	const sub3 = document.getElementById("S3").value;
+	const sub4 = document.getElementById("S4").value;
+	const sub5 = document.getElementById("S5").value;
+	const sub6 = document.getElementById("S6").value;
+	const sub7 = document.getElementById("S7").value;
+	const sub8 = document.getElementById("S8").value;
+	const sub9 = document.getElementById("S9").value;
+
+	const credits = [3, 3, 3, 3, 1.5, 1.5, 3, 1.5, 0];
+	let grades = [sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9];
+
+	const SGPAoutput = document.getElementById("SGPAoutput");
+	const CGPAoutput = document.getElementById("CGPAoutput");
+	const ErrorOutput = document.getElementById("ErrorOutput");
 	
-	const output = document.getElementById("sgpa");
-	
-	const credits = [3, 3, 3, 3, 1.5, 1.5, 3, 1.5, 0]
-	const grades = [sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9]
-	
-	let GPA = 0;
+	let SGPA = 0;
 	let tot_cred = 0;
 	
 	for(i=0; i<credits.length; i++)
 	{
 		tot_cred += credits[i];
-		GPA += grades[i] * credits[i];
+		SGPA += grades[i] * credits[i];
 	}
 	
-	GPA /= tot_cred;
-	
-	output.innerHTML = GPA;
+	SGPA /= tot_cred;
+
+	if(SGPA < 0)
+		ErrorOutput.innerHTML = "**Select Your Grades**";
+
+	else
+	{
+		let CGPA = SGPA;
+		
+		ErrorOutput.innerHTML = "";
+		SGPAoutput.innerHTML = Math.round(SGPA * 100) / 100;
+		CGPAoutput.innerHTML = Math.round(CGPA * 100) / 100;
+	}
 }
 
-function calculateSem2GPA()
+function calculateSem2GPA(Sem)
 {
-	const sub1 = parseFloat(document.getElementById("S1").value);
-	const sub2 = parseFloat(document.getElementById("S2").value);
-	const sub3 = parseFloat(document.getElementById("S3").value);
-	const sub4 = parseFloat(document.getElementById("S4").value);
-	const sub5 = parseFloat(document.getElementById("S5").value);
-	const sub6 = parseFloat(document.getElementById("S6").value);
-	const sub7 = parseFloat(document.getElementById("S7").value);
-	const sub8 = parseFloat(document.getElementById("S8").value);
-	const sub9 = parseFloat(document.getElementById("S9").value);
-	const sub10 = parseFloat(document.getElementById("S10").value);
+	const sub1 = document.getElementById("S1").value;
+	const sub2 = document.getElementById("S2").value;
+	const sub3 = document.getElementById("S3").value;
+	const sub4 = document.getElementById("S4").value;
+	const sub5 = document.getElementById("S5").value;
+	const sub6 = document.getElementById("S6").value;
+	const sub7 = document.getElementById("S7").value;
+	const sub8 = document.getElementById("S8").value;
+	const sub9 = document.getElementById("S9").value;
+	const sub10 = document.getElementById("S10").value;
 	
-	const output = document.getElementById("sgpa");
+	const SGPAoutput = document.getElementById("SGPAoutput");
+	const CGPAoutput = document.getElementById("CGPAoutput");
+	const PreviousCGPA = document.getElementById("CGPA").value;
+	const ErrorOutput = document.getElementById("ErrorOutput");
 	
 	const credits = [3, 3, 3, 3, 2, 1.5, 1, 1.5, 1.5, 0]
 	const grades = [sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9, sub10]
 	
-	let GPA = 0;
+	let SGPA = 0;
 	let tot_cred = 0;
-	
+
 	for(i=0; i<credits.length; i++)
 	{
 		tot_cred += credits[i];
-		GPA += grades[i] * credits[i];
+		SGPA += grades[i] * credits[i];
 	}
-	
-	GPA /= tot_cred;
-	
-	output.innerHTML = GPA;
+
+	SGPA /= tot_cred;
+
+	if(SGPA < 0)
+		ErrorOutput.innerHTML = "**Select Your Grades**";
+
+	else if(PreviousCGPA == "")
+		ErrorOutput.innerHTML = "**Enter Your Previous CGPA**";
+
+	else if(PreviousCGPA < 0 || PreviousCGPA > 10)
+		ErrorOutput.innerHTML = "**Invalid CGPA**"
+
+	else
+	{
+		let CGPA = (parseFloat(PreviousCGPA) * (Sem-1) + SGPA)/Sem;
+		
+		ErrorOutput.innerHTML = "";
+		SGPAoutput.innerHTML = Math.round(SGPA * 100) / 100;
+		CGPAoutput.innerHTML = Math.round(CGPA * 100) / 100;
+	}
 }
 
-function calculateSem3GPA()
+function calculateSem3GPA(Sem)
 {
 	const sub1 = parseFloat(document.getElementById("S1").value);
 	const sub2 = parseFloat(document.getElementById("S2").value);
@@ -74,26 +105,45 @@ function calculateSem3GPA()
 	const sub9 = parseFloat(document.getElementById("S9").value);
 	const sub10 = parseFloat(document.getElementById("S10").value);
 	
-	const output = document.getElementById("sgpa");
+	const SGPAoutput = document.getElementById("SGPAoutput");
+	const CGPAoutput = document.getElementById("CGPAoutput");
+	const PreviousCGPA = document.getElementById("CGPA").value;
+	const ErrorOutput = document.getElementById("ErrorOutput");
 	
 	const credits = [3, 3, 3, 3, 3, 1.5, 1.5, 1.5, 2, 0]
 	const grades = [sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9, sub10]
 	
-	let GPA = 0;
+	let SGPA = 0;
 	let tot_cred = 0;
 	
 	for(i=0; i<credits.length; i++)
 	{
 		tot_cred += credits[i];
-		GPA += grades[i] * credits[i];
+		SGPA += grades[i] * credits[i];
 	}
 	
-	GPA /= tot_cred;
+	SGPA /= tot_cred;
 	
-	output.innerHTML = GPA;
+	if(SGPA < 0)
+		ErrorOutput.innerHTML = "**Select Your Grades**";
+
+	else if(PreviousCGPA == "")
+		ErrorOutput.innerHTML = "**Enter Your Previous CGPA**";
+
+	else if(PreviousCGPA < 0 || PreviousCGPA > 10)
+		ErrorOutput.innerHTML = "**Invalid CGPA**"
+
+	else
+	{
+		let CGPA = (parseFloat(PreviousCGPA) * (Sem-1) + SGPA)/Sem;
+		
+		ErrorOutput.innerHTML = "";
+		SGPAoutput.innerHTML = Math.round(SGPA * 100) / 100;
+		CGPAoutput.innerHTML = Math.round(CGPA * 100) / 100;
+	}
 }
 
-function calculateSem4GPA()
+function calculateSem4GPA(Sem)
 {
 	const sub1 = parseFloat(document.getElementById("S1").value);
 	const sub2 = parseFloat(document.getElementById("S2").value);
@@ -106,26 +156,45 @@ function calculateSem4GPA()
 	const sub9 = parseFloat(document.getElementById("S9").value);
 	const sub10 = parseFloat(document.getElementById("S10").value);
 	
-	const output = document.getElementById("sgpa");
+	const SGPAoutput = document.getElementById("SGPAoutput");
+	const CGPAoutput = document.getElementById("CGPAoutput");
+	const PreviousCGPA = document.getElementById("CGPA").value;
+	const ErrorOutput = document.getElementById("ErrorOutput");
 	
 	const credits = [3, 3, 3, 3, 3, 1.5, 1.5, 1.5, 2, 0]
 	const grades = [sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9, sub10]
 	
-	let GPA = 0;
+	let SGPA = 0;
 	let tot_cred = 0;
 	
 	for(i=0; i<credits.length; i++)
 	{
 		tot_cred += credits[i];
-		GPA += grades[i] * credits[i];
+		SGPA += grades[i] * credits[i];
 	}
 	
-	GPA /= tot_cred;
+	SGPA /= tot_cred;
 	
-	output.innerHTML = GPA;
+	if(SGPA < 0)
+		ErrorOutput.innerHTML = "**Select Your Grades**";
+
+	else if(PreviousCGPA == "")
+		ErrorOutput.innerHTML = "**Enter Your Previous CGPA**";
+
+	else if(PreviousCGPA < 0 || PreviousCGPA > 10)
+		ErrorOutput.innerHTML = "**Invalid CGPA**"
+
+	else
+	{
+		let CGPA = (parseFloat(PreviousCGPA) * (Sem-1) + SGPA)/Sem;
+		
+		ErrorOutput.innerHTML = "";
+		SGPAoutput.innerHTML = Math.round(SGPA * 100) / 100;
+		CGPAoutput.innerHTML = Math.round(CGPA * 100) / 100;
+	}
 }
 
-function calculateSem5GPA()
+function calculateSem5GPA(Sem)
 {
 	const sub1 = parseFloat(document.getElementById("S1").value);
 	const sub2 = parseFloat(document.getElementById("S2").value);
@@ -137,26 +206,45 @@ function calculateSem5GPA()
 	const sub8 = parseFloat(document.getElementById("S8").value);
 	const sub9 = parseFloat(document.getElementById("S9").value);
 	
-	const output = document.getElementById("sgpa");
+	const SGPAoutput = document.getElementById("SGPAoutput");
+	const CGPAoutput = document.getElementById("CGPAoutput");
+	const PreviousCGPA = document.getElementById("CGPA").value;
+	const ErrorOutput = document.getElementById("ErrorOutput");
 	
 	const credits = [3, 3, 3, 3, 3, 1.5, 1.5, 1.5, 2]
 	const grades = [sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9]
 	
-	let GPA = 0;
+	let SGPA = 0;
 	let tot_cred = 0;
 	
 	for(i=0; i<credits.length; i++)
 	{
 		tot_cred += credits[i];
-		GPA += grades[i] * credits[i];
+		SGPA += grades[i] * credits[i];
 	}
 	
-	GPA /= tot_cred;
+	SGPA /= tot_cred;
 	
-	output.innerHTML = GPA;
+	if(SGPA < 0)
+		ErrorOutput.innerHTML = "**Select Your Grades**";
+
+	else if(PreviousCGPA == "")
+		ErrorOutput.innerHTML = "**Enter Your Previous CGPA**";
+
+	else if(PreviousCGPA < 0 || PreviousCGPA > 10)
+		ErrorOutput.innerHTML = "**Invalid CGPA**"
+
+	else
+	{
+		let CGPA = (parseFloat(PreviousCGPA) * (Sem-1) + SGPA)/Sem;
+		
+		ErrorOutput.innerHTML = "";
+		SGPAoutput.innerHTML = Math.round(SGPA * 100) / 100;
+		CGPAoutput.innerHTML = Math.round(CGPA * 100) / 100;
+	}
 }
 
-function calculateSem6GPA()
+function calculateSem6GPA(Sem)
 {
 	const sub1 = parseFloat(document.getElementById("S1").value);
 	const sub2 = parseFloat(document.getElementById("S2").value);
@@ -168,26 +256,45 @@ function calculateSem6GPA()
 	const sub8 = parseFloat(document.getElementById("S8").value);
 	const sub9 = parseFloat(document.getElementById("S9").value);
 	
-	const output = document.getElementById("sgpa");
+	const SGPAoutput = document.getElementById("SGPAoutput");
+	const CGPAoutput = document.getElementById("CGPAoutput");
+	const PreviousCGPA = document.getElementById("CGPA").value;
+	const ErrorOutput = document.getElementById("ErrorOutput");
 	
 	const credits = [3, 3, 3, 3, 3, 1.5, 1.5, 1.5, 2]
 	const grades = [sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8, sub9]
 	
-	let GPA = 0;
+	let SGPA = 0;
 	let tot_cred = 0;
 	
 	for(i=0; i<credits.length; i++)
 	{
 		tot_cred += credits[i];
-		GPA += grades[i] * credits[i];
+		SGPA += grades[i] * credits[i];
 	}
 	
-	GPA /= tot_cred;
+	SGPA /= tot_cred;
 	
-	output.innerHTML = GPA;
+	if(SGPA < 0)
+		ErrorOutput.innerHTML = "**Select Your Grades**";
+
+	else if(PreviousCGPA == "")
+		ErrorOutput.innerHTML = "**Enter Your Previous CGPA**";
+		
+	else if(PreviousCGPA < 0 || PreviousCGPA > 10)
+		ErrorOutput.innerHTML = "**Invalid CGPA**"
+
+	else
+	{
+		let CGPA = (parseFloat(PreviousCGPA) * (Sem-1) + SGPA)/Sem;
+		
+		ErrorOutput.innerHTML = "";
+		SGPAoutput.innerHTML = Math.round(SGPA * 100) / 100;
+		CGPAoutput.innerHTML = Math.round(CGPA * 100) / 100;
+	}
 }
 
-function calculateSem7GPA()
+function calculateSem7GPA(Sem)
 {
 	const sub1 = parseFloat(document.getElementById("S1").value);
 	const sub2 = parseFloat(document.getElementById("S2").value);
@@ -198,44 +305,82 @@ function calculateSem7GPA()
 	const sub7 = parseFloat(document.getElementById("S7").value);
 	const sub8 = parseFloat(document.getElementById("S8").value);
 	
-	const output = document.getElementById("sgpa");
+	const SGPAoutput = document.getElementById("SGPAoutput");
+	const CGPAoutput = document.getElementById("CGPAoutput");
+	const PreviousCGPA = document.getElementById("CGPA").value;
+	const ErrorOutput = document.getElementById("ErrorOutput");
 	
 	const credits = [3, 3, 3, 3, 3, 3, 3, 2]
 	const grades = [sub1, sub2, sub3, sub4, sub5, sub6, sub7, sub8]
 	
-	let GPA = 0;
+	let SGPA = 0;
 	let tot_cred = 0;
 	
 	for(i=0; i<credits.length; i++)
 	{
 		tot_cred += credits[i];
-		GPA += grades[i] * credits[i];
+		SGPA += grades[i] * credits[i];
 	}
 	
-	GPA /= tot_cred;
+	SGPA /= tot_cred;
 	
-	output.innerHTML = GPA;
+	if(SGPA < 0)
+		ErrorOutput.innerHTML = "**Select Your Grades**";
+
+	else if(PreviousCGPA == "")
+		ErrorOutput.innerHTML = "**Enter Your Previous CGPA**";
+
+	else if(PreviousCGPA < 0 || PreviousCGPA > 10)
+		ErrorOutput.innerHTML = "**Invalid CGPA**"
+
+	else
+	{
+		let CGPA = (parseFloat(PreviousCGPA) * (Sem-1) + SGPA)/Sem;
+		
+		ErrorOutput.innerHTML = "";
+		SGPAoutput.innerHTML = Math.round(SGPA * 100) / 100;
+		CGPAoutput.innerHTML = Math.round(CGPA * 100) / 100;
+	}
 }
 
-function calculateSem8GPA()
+function calculateSem8GPA(Sem)
 {
 	const sub1 = parseFloat(document.getElementById("S1").value);
 	
-	const output = document.getElementById("sgpa");
+	const SGPAoutput = document.getElementById("SGPAoutput");
+	const CGPAoutput = document.getElementById("CGPAoutput");
+	const PreviousCGPA = document.getElementById("CGPA").value;
+	const ErrorOutput = document.getElementById("ErrorOutput");
 	
 	const credits = [12]
 	const grades = [sub1]
 	
-	let GPA = 0;
+	let SGPA = 0;
 	let tot_cred = 0;
 	
 	for(i=0; i<credits.length; i++)
 	{
 		tot_cred += credits[i];
-		GPA += grades[i] * credits[i];
+		SGPA += grades[i] * credits[i];
 	}
 	
-	GPA /= tot_cred;
+	SGPA /= tot_cred;
 	
-	output.innerHTML = GPA;
+	if(SGPA < 0)
+		ErrorOutput.innerHTML = "**Select Your Grades**";
+
+	else if(PreviousCGPA == "")
+		ErrorOutput.innerHTML = "**Enter Your Previous CGPA**";
+
+	else if(PreviousCGPA < 0 || PreviousCGPA > 10)
+		ErrorOutput.innerHTML = "**Invalid CGPA**"
+
+	else
+	{
+		let CGPA = (parseFloat(PreviousCGPA) * (Sem-1) + SGPA)/Sem;
+		
+		ErrorOutput.innerHTML = "";
+		SGPAoutput.innerHTML = Math.round(SGPA * 100) / 100;
+		CGPAoutput.innerHTML = Math.round(CGPA * 100) / 100;
+	}
 }
